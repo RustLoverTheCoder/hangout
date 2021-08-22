9d7684769eb28566deac12065bb9e21a1cb1b8a4
ranbo.ma <ranbo.ma@bobosync.com>
2021-08-22 18:56:04 +0800

    feat: add channel gif2

--- src/components/ChannelHeader/index.tsx
+++ src/components/ChannelHeader/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 const ChannelHeader = () => {
   return (
-    <button className="w-full h-7 bg-tertiary rounded text-muted text-sm flex justify-start items-center px-1.5 font-medium">
+    <button className="w-55 h-7 bg-tertiary rounded text-muted text-sm flex justify-start items-center px-1.5 font-medium">
       寻找或开始新的对话
     </button>
   )
