096eb8eceae23687b864b32015b6f514e9a9c207
ranbo.ma <ranbo.ma@bobosync.com>
2021-08-28 14:06:53 +0800

    feat: will add editor

---
+++ src/components/Editor/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+
+const Editor = () => {
+  return (
+    <div>
+      <div>213</div>
+    </div>
+  )
+}
+
+export default Editor
--- src/layouts/ChatContainerLayout.tsx
+++ src/layouts/ChatContainerLayout.tsx
@@ -1,11 +1,17 @@
 import * as React from "react"
+import Editor from "../components/Editor"
 
 interface PropsTypes {
   children?: React.ReactElement | Array<React.ReactElement>
 }
 
 const ChatContainerLayout = ({ children }: PropsTypes) => {
-  return <div>{children}</div>
+  return (
+    <div>
+      {children}
+      <Editor />
+    </div>
+  )
 }
 
 export default ChatContainerLayout
