5cb7ccab6aa1e4a32208149e78117b22c109b0ac
maxiang <maxiang@haoshuo.com>
2021-09-08 17:43:25 +0800

    feat: hidden server

--- src/components/Server/index.tsx
+++ src/components/Server/index.tsx
@@ -5,7 +5,7 @@ import ServerItem from "../ServerItem"
 const Server = () => {
   const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
   return (
-    <div className="flex flex-col w-18 bg-tertiary py-3 overflow-y-auto">
+    <div className="flex-col w-18 bg-tertiary py-3 overflow-y-auto hidden lg:flex">
       {list.map(_ => (
         <ServerItem />
       ))}
