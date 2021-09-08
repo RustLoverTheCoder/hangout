fe52e2e80e5cc0cf6ee1e1bbbd742ffef4422d01
ranbo.ma <ranbo.ma@bobosync.com>
2021-09-08 21:33:47 +0800

    feat: Optimize adaptive

--- src/components/ChatHeader/index.tsx
+++ src/components/ChatHeader/index.tsx
@@ -1,11 +1,12 @@
 import * as React from "react"
-import { IoHelpCircleSharp, IoMail, IoAccessibility } from "react-icons/io5"
+import { IoHelpCircleSharp, IoMail } from "react-icons/io5"
+import { CgMenuLeft } from "react-icons/cg"
 import { Link } from "gatsby"
 
 const ChatHeader = () => {
   return (
     <div className="w-full h-12 bg-primary flex justify-between items-center shadow px-2 text-interactive-normal">
-      <div className="flex">
+      <div className="hidden md:flex">
         <Link
           to="/app/#xx"
           className="px-2 mx-2 h-6 flex justify-center items-center hover:text-interactive-hover font-semibold"
@@ -27,6 +28,9 @@ const ChatHeader = () => {
         <div className="px-2 mx-2 h-6 flex justify-center items-center hover:text-interactive-hover font-semibold">
           添加好友
         </div>
+      </div>
+      <div className="w-10 flex justify-center items-center md:hidden">
+        <CgMenuLeft className="text-2xl hover:text-interactive-hover" />
       </div>
       <div className="flex">
         <div className="w-10 flex justify-center items-center">
