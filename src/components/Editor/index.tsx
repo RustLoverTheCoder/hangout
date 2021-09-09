8ef644b92a8c88c36ab265c1cf64ba985816f006
ranbo.ma <ranbo.ma@bobosync.com>
2021-09-09 20:31:44 +0800

    feat: test MarkdownEditor

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,67 +1,57 @@
 import "remirror/styles/all.css"
 import * as React from "react"
 const isBrowser = typeof window !== "undefined"
-import {
-  BoldExtension,
-  ItalicExtension,
-  PlaceholderExtension
-} from "remirror/extensions"
-import { Remirror, useRemirror, ReactExtensions } from "@remirror/react"
+import { PlaceholderExtension } from "remirror/extensions"
+import { Remirror, useRemirror } from "@remirror/react"
 import { IoAddCircleSharp } from "react-icons/io5"
 import { BiHappy, BiSticker, BiGift } from "react-icons/bi"
 import { AiOutlineGif } from "react-icons/ai"
-import { RiSendPlaneFill } from 'react-icons/ri'
-
-
-
+import { RiSendPlaneFill } from "react-icons/ri"
 
 const Editor = () => {
-
-
-  const extensions = React.useCallback(() => [
-    new BoldExtension(),
-    new ItalicExtension(),
-    new PlaceholderExtension({ placeholder: `Hi!` })
-  ], [])
+  const extensions = React.useCallback(
+    () => [new PlaceholderExtension({ placeholder: `Hi!` })],
+    []
+  )
 
   const { manager } = useRemirror({
     extensions,
-    selection: 'end',
-    stringHandler: 'html',
+    selection: "end",
+    stringHandler: "html",
+
   })
 
-  return !isBrowser && (
-    <div
-      className="w-full rounded-lg flex items-start"
-      style={{ background: "#40444b", minHeight: "44px" }}
-    >
-      <div className="w-14 h-11 flex justify-center items-center flex-shrink-0">
-        <IoAddCircleSharp className="text-2xl text-interactive-normal hover:text-interactive-hover" />
+  return (
+    isBrowser && (
+      <div
+        className="w-full rounded-lg flex items-start"
+        style={{ background: "#40444b", minHeight: "44px" }}
+      >
+        <div className="w-14 h-11 flex justify-center items-center flex-shrink-0">
+          <IoAddCircleSharp className="text-2xl text-interactive-normal hover:text-interactive-hover" />
+        </div>
+        <div className="flex-1 text-interactive-normal py-2.5">
+          <Remirror manager={manager} classNames={["outline-none"]} />
+        </div>
+        <div className="flex flex-shrink-0 h-11">
+          <div className="w-11 h-11 flex justify-center items-center md:hidden">
+            <RiSendPlaneFill className="text-2xl text-interactive-normal hover:text-interactive-hover" />
+          </div>
+          <div className="w-11 h-11  hidden md:flex justify-center items-center">
+            <BiGift className="text-2xl text-interactive-normal hover:text-interactive-hover" />
+          </div>
+          <div className="w-11 h-11 hidden md:flex justify-center items-center">
+            <AiOutlineGif className="text-2xl text-interactive-normal hover:text-interactive-hover" />
+          </div>
+          <div className="w-11 h-11 hidden md:flex justify-center items-center">
+            <BiSticker className="text-2xl text-interactive-normal hover:text-interactive-hover" />
+          </div>
+          <div className="w-11 h-11 hidden md:flex justify-center items-center">
+            <BiHappy className="text-2xl text-interactive-normal hover:text-interactive-hover" />
+          </div>
+        </div>
       </div>
-      <div className="flex-1 text-interactive-normal py-2.5">
-        <Remirror
-          manager={manager}
-          classNames={["outline-none"]}
-        />
-      </div>
-      <div className="flex flex-shrink-0 h-11">
-        <div className="w-11 h-11 flex justify-center items-center md:hidden">
-          <RiSendPlaneFill className="text-2xl text-interactive-normal hover:text-interactive-hover" />
-        </div>
-        <div className="w-11 h-11  hidden md:flex justify-center items-center">
-          <BiGift className="text-2xl text-interactive-normal hover:text-interactive-hover" />
-        </div>
-        <div className="w-11 h-11 hidden md:flex justify-center items-center">
-          <AiOutlineGif className="text-2xl text-interactive-normal hover:text-interactive-hover" />
-        </div>
-        <div className="w-11 h-11 hidden md:flex justify-center items-center">
-          <BiSticker className="text-2xl text-interactive-normal hover:text-interactive-hover" />
-        </div>
-        <div className="w-11 h-11 hidden md:flex justify-center items-center">
-          <BiHappy className="text-2xl text-interactive-normal hover:text-interactive-hover" />
-        </div>
-      </div>
-    </div>
+    )
   )
 }
 
--- src/layouts/ChatContainerLayout.tsx
+++ src/layouts/ChatContainerLayout.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import Editor from "../components/Editor"
+import { MarkdownEditor } from '@remirror/react-editors/markdown';
 
 interface PropsTypes {
   children?: React.ReactElement | Array<React.ReactElement>
@@ -8,7 +9,12 @@ interface PropsTypes {
 const ChatContainerLayout = ({ children }: PropsTypes) => {
   return (
     <div className="flex flex-col justify-between w-full h-full">
-      <div>{children}</div>
+      <div>
+        {children}
+        <div className="w-full px-4 mb-6">
+          <MarkdownEditor placeholder='Hi!' />
+        </div>
+      </div>
       <div className="w-full px-4 mb-6">
         <Editor />
       </div>
