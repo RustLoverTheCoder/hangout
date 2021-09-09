8e62fb39060970ba2c70cdb573ce1868bdd344da
ranbo.ma <ranbo.ma@bobosync.com>
2021-09-09 21:56:37 +0800

    feat: test MarkdownEditor4

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -9,54 +9,46 @@ import { BiHappy, BiSticker, BiGift } from "react-icons/bi"
 import { AiOutlineGif } from "react-icons/ai"
 import { RiSendPlaneFill } from "react-icons/ri"
 
-const extensions = () => [new BoldExtension(), new ItalicExtension(), new UnderlineExtension()]
+const extensions = () => [new BoldExtension(), new ItalicExtension(), new UnderlineExtension(), new PlaceholderExtension({ placeholder: "hi" })]
 
 const Editor = () => {
-  if(!isBrowser) return null
-  // const extensions = React.useCallback(
-  //   () => [new PlaceholderExtension({ placeholder: "<p>Hi</p>" }), new BoldExtension(), new ItalicExtension(), new UnderlineExtension()],
-  //   []
-  // )
+  if (!isBrowser) return null
 
-  const { manager } = useRemirror({
+  const { manager, state, onChange } = useRemirror({
     extensions,
     selection: "end",
-    stringHandler: "html",
-    content: "<p>Hi <strong>Friend</strong></p>"
+    stringHandler: "html"
   })
 
   return (
     <div
-      className="w-full rounded-lg"
-      // style={{ background: "#40444b", minHeight: "44px" }}
+      className="w-full rounded-lg flex items-start"
+      style={{ background: "#40444b", minHeight: "44px" }}
     >
-      {/*<div className="flex-1 text-interactive-normal py-2.5 bg-white">*/}
-      {/*</div>*/}
-      <Remirror manager={manager} />
-
-      {/*<div className="w-14 h-11 flex justify-center items-center flex-shrink-0">*/}
-      {/*  <IoAddCircleSharp className="text-2xl text-interactive-normal hover:text-interactive-hover" />*/}
-      {/*</div>*/}
-      {/*<div className="flex-1 text-interactive-normal py-2.5">*/}
-      {/*  <Remirror manager={manager} classNames={["outline-none"]} />*/}
-      {/*</div>*/}
-      {/*<div className="flex flex-shrink-0 h-11">*/}
-      {/*  <div className="w-11 h-11 flex justify-center items-center md:hidden">*/}
-      {/*    <RiSendPlaneFill className="text-2xl text-interactive-normal hover:text-interactive-hover" />*/}
-      {/*  </div>*/}
-      {/*  <div className="w-11 h-11  hidden md:flex justify-center items-center">*/}
-      {/*    <BiGift className="text-2xl text-interactive-normal hover:text-interactive-hover" />*/}
-      {/*  </div>*/}
-      {/*  <div className="w-11 h-11 hidden md:flex justify-center items-center">*/}
-      {/*    <AiOutlineGif className="text-2xl text-interactive-normal hover:text-interactive-hover" />*/}
-      {/*  </div>*/}
-      {/*  <div className="w-11 h-11 hidden md:flex justify-center items-center">*/}
-      {/*    <BiSticker className="text-2xl text-interactive-normal hover:text-interactive-hover" />*/}
-      {/*  </div>*/}
-      {/*  <div className="w-11 h-11 hidden md:flex justify-center items-center">*/}
-      {/*    <BiHappy className="text-2xl text-interactive-normal hover:text-interactive-hover" />*/}
-      {/*  </div>*/}
-      {/*</div>*/}
+      <div className="w-14 h-11 flex justify-center items-center flex-shrink-0">
+        <IoAddCircleSharp className="text-2xl text-interactive-normal hover:text-interactive-hover" />
+      </div>
+      <div className="flex-1 text-interactive-normal py-2.5">
+        <Remirror manager={manager} state={state} onChange={onChange} placeholder={"hi"}
+                  classNames={["outline-none"]} />
+      </div>
+      <div className="flex flex-shrink-0 h-11">
+        <div className="w-11 h-11 flex justify-center items-center sm:hidden">
+          <RiSendPlaneFill className="text-2xl text-interactive-normal hover:text-interactive-hover" />
+        </div>
+        <div className="w-11 h-11  hidden sm:flex justify-center items-center">
+          <BiGift className="text-2xl text-interactive-normal hover:text-interactive-hover" />
+        </div>
+        <div className="w-11 h-11 hidden sm:flex justify-center items-center">
+          <AiOutlineGif className="text-2xl text-interactive-normal hover:text-interactive-hover" />
+        </div>
+        <div className="w-11 h-11 hidden sm:flex justify-center items-center">
+          <BiSticker className="text-2xl text-interactive-normal hover:text-interactive-hover" />
+        </div>
+        <div className="w-11 h-11 hidden sm:flex justify-center items-center">
+          <BiHappy className="text-2xl text-interactive-normal hover:text-interactive-hover" />
+        </div>
+      </div>
     </div>
   )
 }
