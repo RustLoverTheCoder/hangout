d0acc28a66a8cf58db715037392acdcd5f4d97dc
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2021-09-16 22:36:21 +0800

    Update page-2.tsx

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -1,8 +1,12 @@
 import * as React from "react"
+const isBrowser = typeof window !== "undefined"
 import { StaticImage } from "gatsby-plugin-image"
 
 
 const SecondPage = () => {
+  if(!isBrowser){
+    return null
+  }
   return (
     <div className="w-full h-full flex justify-center items-center">
       {window.innerWidth < 800 ? <StaticImage src="../images/home_bg_mini.png"
