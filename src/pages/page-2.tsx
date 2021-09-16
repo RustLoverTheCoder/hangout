5a30ea916efe437e2ebe571dec3875bd9688faa0
ranbo.ma <ranbo.ma@bobosync.com>
2021-09-16 21:48:58 +0800

    feat: add new image bg

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -2,13 +2,17 @@ import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 
 
-
-
 const SecondPage = () => {
-
   return (
-    <div className='w-full h-full flex justify-center items-center'>
-      <StaticImage src='../images/home_bg.webp' width={window.innerWidth} quality={75} className='h-full object-cover md:w-full' formats={["AUTO", "WEBP", "AVIF"]} alt="背景" />
+    <div className="w-full h-full flex justify-center items-center">
+      {window.innerWidth < 800 ? <StaticImage src="../images/home_bg_mini.png"
+                                              width={window.innerWidth} quality={75}
+                                              className="h-full object-cover md:w-full"
+                                              formats={["AUTO", "WEBP", "AVIF"]} alt="背景" /> :
+        <StaticImage src="../images/home_bg.png"
+                     width={window.innerWidth} quality={75} className="h-full object-cover md:w-full"
+                     formats={["AUTO", "WEBP", "AVIF"]} alt="背景" />}
+
     </div>
   )
 }
