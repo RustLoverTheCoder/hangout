3badd1161cf779789b6ff907a8d4c4e204d92c8b
maxiang <maxiang@haoshuo.com>
2021-09-16 18:35:41 +0800

    feat: home bg

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -1,11 +1,14 @@
 import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+
 
 
 
 const SecondPage = () => {
+
   return (
-    <div>
-      <div>123123</div>
+    <div className='w-full h-full flex justify-center items-center'>
+      <StaticImage src='../images/home_bg.webp' width={window.innerWidth} quality={75} className='h-full object-cover md:w-full' formats={["AUTO", "WEBP", "AVIF"]} alt="背景" />
     </div>
   )
 }
