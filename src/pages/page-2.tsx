8c9256dd5d28623e7f5fe5e200f76a6e940c7c07
maxiang <maxiang@haoshuo.com>
2021-09-17 10:56:54 +0800

    feat: upgrade gatsby v4

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -7,12 +7,32 @@ type DataProps = {
   }
 }
 
-const AppPage: React.FC<PageProps<DataProps>> = ({ data, path }) => {
+const AppPage: React.FC<PageProps<DataProps>> = ({ data, path, serverData }) => {
   return (
-    <div>
-      <div>123</div>
-    </div>
+    <main>
+      <h1>SSR Page with Dogs</h1>
+      <img alt="Happy dog" src={serverData.message} />
+    </main>
   )
 }
 
 export default AppPage
+
+export async function getServerData() {
+  try {
+    const res = await fetch(`https://dog.ceo/api/breeds/image/random`)
+    if (!res.ok) {
+      throw new Error(`Response failed`)
+    }
+    return {
+      props: await res.json(),
+    }
+  } catch (error) {
+    return {
+      headers: {
+        status: 500,
+      },
+      props: {},
+    }
+  }
+}
