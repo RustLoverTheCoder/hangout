dcc114ec74b055dccd508bb187ec65ed5971b9e4
maxiang <maxiang@haoshuo.com>
2021-09-22 09:51:55 +0800

    feat: add new theme_color

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,11 +32,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `哔啵`,
+        short_name: `哔啵`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#36393f`,
+        theme_color: `#36393f`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
