014f3db659f96c0c7e32d99ed2aa844f051f3993
maxiang <maxiang@haoshuo.com>
2021-10-20 11:31:08 +0800

    feat: link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,9 @@ import * as React from "react"
 import { PageProps } from "gatsby"
 import HomeBgLayout from "../layouts/HomeBgLayout"
 import Seo from "../components/seo"
-import { Link, navigate } from "gatsby"
+import { Link } from "gatsby"
 
 import NickNameInput from "../components/NickNameInput"
-
-const isBrowser = typeof window !== "undefined"
 
 
 type DataProps = {
@@ -17,26 +15,26 @@ type DataProps = {
 
 const IndexPage: React.FC<PageProps<DataProps>> = ({ data, path }) => {
   const [showInput, setShowInput] = React.useState(false)
-  const handleOpenApp = () => {
-    if (!isBrowser) return false
-    const token = localStorage.getItem("access_token")
-    if (!token) {
-      setShowInput(true)
-    } else {
-      navigate("/app")
-    }
-  }
+  // const handleOpenApp = () => {
+  //   if (!isBrowser) return false
+  //   const token = localStorage.getItem("access_token")
+  //   if (!token) {
+  //     setShowInput(true)
+  //   } else {
+  //     navigate("/app")
+  //   }
+  // }
   return (
     <HomeBgLayout>
       <Seo title="阿瓦隆" />
       <div className="w-full h-full flex justify-center items-center">
         <div className="container flex justify-center md:justify-start items-center px-6">
           {showInput ? <NickNameInput /> : (
-            <button
-              onClick={() => handleOpenApp()}
+            <Link
+              to={"/login"}
               className="bg-quite-black text-xl text-white px-8 py-4 mt-6 rounded-full font-semibold">
               在你的浏览器中打开app
-            </button>
+            </Link>
           )}
         </div>
       </div>
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { PageProps } from "gatsby"
 import Seo from "../components/seo"
+import { Link } from "gatsby"
 
 type DataProps = {
   site: {
@@ -13,7 +14,8 @@ const LoginPage: React.FC<PageProps<DataProps>> = ({ data, path }) => {
     <div className="w-full h-full bg-brand flex justify-center items-center">
       <Seo title="阿瓦隆" />
       <div className="container flex justify-center items-center px-4 sm:mx-0">
-        <div className="w-full sm:w-auto h-102 md:w-196 md:h-102 bg-primary text-muted p-8 rounded flex justify-between">
+        <div
+          className="w-full sm:w-auto h-102 md:w-196 md:h-102 bg-primary text-muted p-8 rounded flex justify-between">
           <div className="h-full w-full sm:w-100 flex flex-col">
             <h3 className="text-2xl text-white font-semibold mb-2">欢迎回来！</h3>
             <div className="text-base leading-5 text-header-secondary">很高兴再次见到你！</div>
@@ -26,10 +28,11 @@ const LoginPage: React.FC<PageProps<DataProps>> = ({ data, path }) => {
                 <div className="mb-2 text-xs leading-4">验证码</div>
                 <div className="w-full h-10.5 bg-text-input-bg rounded border border-gray-600"></div>
               </div>
-              <button
+              <Link
+                to={"/app"}
                 className="w-full h-11 text-base leading-6 text-white mt-11 bg-brand rounded font-medium px-4 py-0.5">
                 登录
-              </button>
+              </Link>
               <div className="mt-0.5">
                 <span className="text-sm leading-4">没有账号即注册</span>
               </div>
