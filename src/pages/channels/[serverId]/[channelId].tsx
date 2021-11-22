97319c28cfaace2bd6f20a394fb57e1134178240
maxiang <maxiang@haoshuo.com>
2021-11-22 09:51:00 +0800

    feat: splat router

---
+++ src/pages/channels/[serverId]/[channelId].tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const Channel: React.FC = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>Channel</h1>
+    </div>
+  )
+}
+
+export default Channel
